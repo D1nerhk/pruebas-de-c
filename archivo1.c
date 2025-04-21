6f7e6674ab0725120ed2c6eaff3701ca5c234d50
Dinerhk <dinerhk@SylvieGod.ipv6.infinitum.net.mx>
2025-04-21 13:30:30 -0600

    primer commit

---
+++ archivo1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+int a,b,c;
+
+printf("este es un archivo de prueba!\n");
+
+a=1;
+
+printf("%d",a);
+
+b=2;
+
+printf("%d",b);
+
+c=3;
+
+printf("%d",c);
+ 
+ 
+return 0;
+}
+
